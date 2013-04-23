15449faf6356a92d1d28f5d72e72898a85a19461
Natasha Badillo and Connor Savage <natasha+connor@appacademy.io>
2013-04-23 14:45:12 -0400

    New board stuff

--- board.rb
+++ board.rb
@@ -1,14 +1,25 @@
+require "piece"
+require "knight"
+
 class Board
 
-  attr_accessor :size, :grid, :wh_pieces, :bl_pieces
+  attr_accessor :size, :grid
 
   def initialize(size = 8)
     @size = size
     @grid = Array.new(size) {Array.new(size)}
-    @wh_pieces, @bl_pieces = [], []
   end
 
   def board_setup
+    set_pawns
+    #set_backrows
+  end
+
+  def set_pawns
+    self.size.times do |i|
+      @grid[1][i] = Knight.new([1,i], :wh)
+      @grid[-2][i] = Knight.new([-2,i], :bl)
+    end
 
   end
 
@@ -16,9 +27,25 @@ class Board
     p @grid
   end
 
-  def board_move
-    # move piece on board
-    # call move method on piece
+  def board_move(piece)
+
+    # NOT BLOODY READY
+    # begin
+    #   piece.move(dest, self)
+    # rescue IllegalMove
+    #   puts "Illegal Move"
+    # end
+  end
+
+  def board_remove
+  end
+
+  def setup_pieces
+  end
+
+  def spot(pos)
+    x, y = pos
+    self.grid[x][y]
   end
 
 end
--- knight.rb
+++ knight.rb
@@ -16,17 +16,24 @@ class Knight < Piece
   end
 
   def poss_moves(board)
-    valid_m = []
+    valid_m = check_onboard(board)
 
-    DELTAS.each do |dx,dy|
-      if (0..8).include?( pos[0]+dx) && (0..8).include?( pos[1]+dy)
-        valid_m << [pos[0] + dx, pos[1] + dy]
-      end
-    end
-
+    valid_m.delete_if {|pos| board.spot(pos).color == self.color  }
+    # add check_check
 
   end
 
+  def check_onboard(board)
+    valid_m=[]
+
+    DELTAS.each do |dx,dy|
+      if (0..board.size).include?( self.pos[0]+dx) && (0..board.size).include?( self.pos[1]+dy)
+        valid_m << [self.pos[0] + dx, self.pos[1] + dy]
+      end
+    end
+
+    valid_m
+  end
 
 
 end
